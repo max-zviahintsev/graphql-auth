95e3a03126415fd04c3b4f3fa49c8469586f7d85
Max Zviagintsev <maxim.zviagintsev@gmail.com>
2025-02-14 18:05:10 +0200

    Set up packages, ESlint, TypeScript and Prettier

---
+++ server/eslint.config.js
@@ -0,0 +1,54 @@
+import eslintJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  eslintJs.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'no-unused-expressions': 'error',
+      'no-unused-locals': 'error',
+      'no-unused-parameters': 'error',
+      'no-fallthrough': 'error',
+      'no-unchecked-import': 'error',
+      'consistent-return': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-param-reassign': 'error',
+      'no-else-return': 'error',
+      'no-useless-return': 'error',
+      'no-useless-constructor': 'error',
+      'no-duplicate-imports': 'error',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+      'no-undef': 'off',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+]
